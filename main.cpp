b0236e1be1810d925d2e12d1c30c02937e36f8dd
Shawn Marshall-Spitzbart <smarshall-spitzbart@berkeley.edu>
2021-11-18 04:19:49 -0800

    whoops

--- main.cpp
+++ main.cpp
@@ -12,8 +12,8 @@
 int main() {
 
     // debug your alg against these numbers. 
-    std::string firstInt = "12";
-    std::string secondInt = "45";
+    std::string firstInt = "3141592653589793238462643383279502884197169399375105820974944592";
+    std::string secondInt = "2718281828459045235360287471352662497757247093699959574966967627";
 
     IntegerMultiplier intMultiplier; 
     std::string result = intMultiplier.GradeSchoolMultiplication(firstInt, secondInt);
